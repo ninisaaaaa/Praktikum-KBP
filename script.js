cbdd603c7fc02e2f3b86f2f49ca0ba5db73bf110
ninisaaaaa <kerjaku1409@gmail.com>
2024-12-05 06:10:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+const hearder1 = document.getElementById("h1") //elemen node
+const p = document.getElementsByTagName("p") //html collection
+const list = document.getElementsByClassName("list") //html collection
